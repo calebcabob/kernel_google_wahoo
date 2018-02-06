9ca814a398d91749d3f4e09acd639b3a29982f0c
matthewdalex <matthewalex@outlook.com>
2018-02-06 17:28:28 -0500

    build: R12

--- build.sh
+++ build.sh
@@ -12,14 +12,14 @@ clear
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 KERNEL="Image"
 DTBIMAGE="dtb"
-export CLANG_PATH=~/android/clang/clang-4556391/bin/
+export CLANG_PATH=~/android/clang/clang-4579689/bin/
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=${HOME}/android/uberbuild/out/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 DEFCONFIG="snoke_defconfig"
 
 # Kernel Details
-VER=".R11"
+VER=".R12"
 
 # Paths
 KERNEL_DIR=`pwd`
