ac038bf10e5334c6b125fdcb7c177a5453de56e3
matthewdalex <matthewalex@outlook.com>
2018-02-14 19:21:55 -0500

    build: R14

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ export CROSS_COMPILE=${HOME}/android/uberbuild/out/aarch64-linux-android-4.9/bin
 DEFCONFIG="snoke_defconfig"
 
 # Kernel Details
-VER=".R13R"
+VER=".R14"
 
 # Paths
 KERNEL_DIR=`pwd`
