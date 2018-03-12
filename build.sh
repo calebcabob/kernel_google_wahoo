db92508025860ed7a4b1f93654640aa65fea3dca
matthewdalex <matthewalex@outlook.com>
2018-03-12 12:41:40 -0400

    build: R21

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ clear
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 KERNEL="Image"
 DTBIMAGE="dtb"
-export CLANG_PATH=~/android/clang/clang-4639204/bin/
+export CLANG_PATH=~/android/opt/clang-7.x/bin/
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=${HOME}/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
@@ -20,7 +20,7 @@ export CROSS_COMPILE_ARM32=${HOME}/android/arm-linux-androideabi-4.9/bin/arm-lin
 DEFCONFIG="snoke_defconfig"
 
 # Kernel Details
-VER=".R20"
+VER=".R21"
 
 # Paths
 KERNEL_DIR=`pwd`
